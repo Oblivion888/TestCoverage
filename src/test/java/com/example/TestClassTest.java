970106a0d8b1d114315865ca23f06d3c74bc5125
ilarybalko <rybalko94ia@gmail.com>
2024-07-16 22:14:20 +0500

    full test

--- src/test/java/com/example/TestClassTest.java
+++ src/test/java/com/example/TestClassTest.java
@@ -8,13 +8,13 @@ class TestClassTest {
 
     @Test
     void sum() {
-//        TestClass testClass = new TestClass();
-//
-//        int a = 2;
-//        int b = 2;
-//
-//        int sum = testClass.sum(a, b);
-//
-//        assertEquals(4, sum);
+        TestClass testClass = new TestClass();
+
+        int a = 2;
+        int b = 2;
+
+        int sum = testClass.sum(a, b);
+
+        assertEquals(4, sum);
     }
 }
